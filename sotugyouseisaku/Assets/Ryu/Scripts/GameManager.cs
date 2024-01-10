943adc9817d54665dafd3bf2b421b028caca17a5
RyuKento <83810925+RyuKento@users.noreply.github.com>
2024-01-11 04:02:51 +0900

    [add,update]作業用インゲームシーンの追加、スカイボックスの変更、照明の照度を0にした、個人用ファイルの追加

---
+++ sotugyouseisaku/Assets/Ryu/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>Œ©‚Â‚¯‚½ƒAƒCƒeƒ€‚Ì”</summary>
+    private int _itemCount = 0;
+
+    public int ItemCount { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
